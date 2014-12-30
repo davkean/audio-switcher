68823af36c69c837bc3a20521fbfc291d1cb9d49
David Kean <dave@keansbox.com>
2014-12-30 15:50:20 -0800

    Sub menus no longer appear on a different screen to the parent.
    Fixes #3. WinForms doesn't handle the case where a sub menu lands on a different screen to it's parent. This changes AudioToolStripMenuItem to handle that case.

--- src/AudioSwitcher/AudioSwitcher/Presentation/UI/AudioToolStripMenuItem.cs
+++ src/AudioSwitcher/AudioSwitcher/Presentation/UI/AudioToolStripMenuItem.cs
@@ -10,6 +10,7 @@ namespace AudioSwitcher.Presentation.UI
     internal class AudioToolStripMenuItem : ToolStripMenuItem
     {
         private Image _checkedImage;
+        private bool _doNotCheckDropDownLocation;
 
         public AudioToolStripMenuItem()
         {
@@ -25,6 +26,37 @@ namespace AudioSwitcher.Presentation.UI
                     _checkedImage = value;
                     Invalidate();
                 }
+            }
+        }
+
+        protected override Point DropDownLocation
+        {
+            get
+            {
+                // BUG #3: The default location of a menu's drop down can appear on a different screen than the parent
+                // of that menu. To prevent that, we prefer the reverse side of the current direction, if that results 
+                // in the drop down appearing on the same screen.
+                
+
+                Point location = base.DropDownLocation;
+                if (_doNotCheckDropDownLocation || Parent == null || UIServices.BelongToSameScreen(location, Parent.Location))
+                    return location;
+
+                // Unfortunately, ToolStripDownDownItem.GetDropDownBounds isn't public, and there doesn't appear
+                // to be a better place to hook onto this, so we need to modify state on the MenuItem (and then 
+                // reset it) to mimic this method.
+
+                _doNotCheckDropDownLocation = true; // Prevent cycles
+                ToolStripDropDownDirection currentDirection = DropDownDirection;
+                DropDownDirection = UIServices.FlipDirectionOnXAxis(currentDirection);
+                Point newLocation = base.DropDownLocation;
+
+                if (UIServices.BelongToSameScreen(newLocation, Parent.Location))
+                    location = newLocation; // Better match
+
+                DropDownDirection = currentDirection;
+                _doNotCheckDropDownLocation = false;
+                return location;
             }
         }
 
---
+++ src/AudioSwitcher/AudioSwitcher/Presentation/UI/UIServices.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) David Kean. All rights reserved.
+// -----------------------------------------------------------------------
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AudioSwitcher.Presentation.UI
+{
+    internal static class UIServices
+    {
+        public static ToolStripDropDownDirection FlipDirectionOnXAxis(ToolStripDropDownDirection direction)
+        {
+            switch (direction)
+            {
+                case ToolStripDropDownDirection.Left:
+                    return ToolStripDropDownDirection.Right;
+
+                case ToolStripDropDownDirection.Right:
+                    return ToolStripDropDownDirection.Left;
+
+                case ToolStripDropDownDirection.AboveLeft:
+                    return ToolStripDropDownDirection.AboveRight;
+
+                    case ToolStripDropDownDirection.AboveRight:
+                    return ToolStripDropDownDirection.AboveLeft;
+
+                case ToolStripDropDownDirection.BelowLeft:
+                    return ToolStripDropDownDirection.BelowRight;
+
+                default:
+                    Debug.Assert(direction == ToolStripDropDownDirection.BelowRight);
+                    return ToolStripDropDownDirection.BelowLeft;
+            }
+        }
+
+        public static bool BelongToSameScreen(Point point1, Point point2)
+        {
+            Screen screen1 = Screen.FromPoint(point1);
+            Screen screen2 = Screen.FromPoint(point2);
+
+            return screen1.Equals(screen2);
+        }
+    }
+}
