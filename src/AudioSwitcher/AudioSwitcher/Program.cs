eac7b8c36ed9918823f714aea75f220a5701f9c7
David Kean <dave@keansbox.com>
2014-12-31 09:53:17 -0800

    Removed unused parameter.

--- src/AudioSwitcher/AudioSwitcher/Program.cs
+++ src/AudioSwitcher/AudioSwitcher/Program.cs
@@ -10,7 +10,7 @@ namespace AudioSwitcher
 {
     internal class Program
     {
-        public static void Main(string[] parameters)
+        public static void Main()
         {
             var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
 
