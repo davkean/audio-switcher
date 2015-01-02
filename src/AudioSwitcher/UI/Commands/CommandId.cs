12099cbede916a911717f262db507acb66aa6b57
David Kean <dave@keansbox.com>
2015-01-02 15:49:02 -0800

    Fixed formatting of CommandIds

--- src/AudioSwitcher/UI/Commands/CommandId.cs
+++ src/AudioSwitcher/UI/Commands/CommandId.cs
@@ -7,23 +7,23 @@ namespace AudioSwitcher.UI.Commands
 {
     internal static class CommandId
     {
-        public const string AutomaticallySwitchToPluggedInDevice  = "Settings.AutomaticallySwitchToPluggedInDevice";
-        public const string RunAtWindowsStartup                   = "Settings.RunAtWindowsStartup";
+        public const string AutomaticallySwitchToPluggedInDevice =  "Settings.AutomaticallySwitchToPluggedInDevice";
+        public const string RunAtWindowsStartup =                   "Settings.RunAtWindowsStartup";
 
-        public const string ShowPlaybackDevices                   = "Appearance.ShowPlaybackDevices";
-        public const string ShowRecordingDevices                  = "Appearance.ShowRecordingDevices";
-        
-        public const string ShowDisabledDevices                   = "Appearance.ShowDisabledDevices";
-        public const string ShowNotPresentDevices                 = "Appearance.ShowNotPresentDevices";
-        public const string ShowUnpluggedDevices                  = "Appearance.ShowUnpluggedDevices";
+        public const string ShowPlaybackDevices =                   "Appearance.ShowPlaybackDevices";
+        public const string ShowRecordingDevices =                  "Appearance.ShowRecordingDevices";
 
-        public const string Exit                                        = "Application.Exit";
+        public const string ShowDisabledDevices =                   "Appearance.ShowDisabledDevices";
+        public const string ShowNotPresentDevices =                 "Appearance.ShowNotPresentDevices";
+        public const string ShowUnpluggedDevices =                  "Appearance.ShowUnpluggedDevices";
 
-        public const string SetAsDefaultCommunicationDevice             = "Device.SetAsDefaultCommunicationDevice";
-        public const string SetAsDefaultMultimediaDevice                = "Device.SetAsDefaultMultimediaDevice";
-        public const string SetAsDefaultDevice                          = "Device.SetAsDefaultDevice";
-        public const string NoDevices                                   = "Device.NoDevices";
-        public const string NoRecordingDevices                          = "Device.NoRecordingDevices";
-        public const string NoPlaybackDevices                           = "Device.NoPlaybackDevices";
+        public const string Exit =                                  "Application.Exit";
+
+        public const string SetAsDefaultCommunicationDevice =       "Device.SetAsDefaultCommunicationDevice";
+        public const string SetAsDefaultMultimediaDevice =          "Device.SetAsDefaultMultimediaDevice";
+        public const string SetAsDefaultDevice =                    "Device.SetAsDefaultDevice";
+        public const string NoDevices =                             "Device.NoDevices";
+        public const string NoRecordingDevices =                    "Device.NoRecordingDevices";
+        public const string NoPlaybackDevices =                     "Device.NoPlaybackDevices";
     }
 }
