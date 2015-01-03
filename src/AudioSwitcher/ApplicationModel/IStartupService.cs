52cee5b466788b9c67a5eb09c07c3a10a3888049
David Kean <dave@keansbox.com>
2015-01-02 17:28:10 -0800

    Added a XML comment to IStartupService

--- src/AudioSwitcher/ApplicationModel/IStartupService.cs
+++ src/AudioSwitcher/ApplicationModel/IStartupService.cs
@@ -8,6 +8,12 @@ namespace AudioSwitcher.ApplicationModel
     // Indicates that a service that runs at application startup
     internal interface IStartupService
     {
+        /// <summary>
+        ///     Called to indicate that the application has started.
+        /// </summary>
+        /// <returns>
+        ///     <see langword="true"/> if the application can start; otherwise, <see langword="false"/>.
+        /// </returns>
         bool Startup();
     }
 }
