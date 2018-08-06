7f4509c3e0c239b09c184087540748198d8eea52
David Kean <davkean@microsoft.com>
2018-08-06 13:41:46 +1000

    Add test project

--- src/AudioSwitcher/Properties/AssemblyInfo.cs
+++ src/AudioSwitcher/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 // -----------------------------------------------------------------------
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AudioSwitcher")]
@@ -17,3 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
+[assembly: InternalsVisibleTo("AudioSwitcher.Tests")]
