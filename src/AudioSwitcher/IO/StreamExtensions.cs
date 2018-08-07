8873350d85f53fdd5bd18358ab92bb9d539dabd0
David Kean <davkean@microsoft.com>
2018-08-07 19:28:57 +1000

    Apply things that need to be fixed.

--- src/AudioSwitcher/IO/StreamExtensions.cs
+++ src/AudioSwitcher/IO/StreamExtensions.cs
@@ -2,19 +2,20 @@
 // Copyright (c) David Kean. All rights reserved.
 // -----------------------------------------------------------------------
 using System;
+using System.Windows;
+using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 
 namespace AudioSwitcher.IO
 {
     internal static class StreamExtensions
     {
-        public static T Read<T>(this Stream stream) where T : struct
+        static internal T Read<T>(this Stream stream) where T : struct
         {
-            int size = Marshal.SizeOf(typeof(T));
+            var size = Marshal.SizeOf(typeof(T));
             byte[] buffer = new byte[size];
-            int bytesRead = stream.Read(buffer, 0, size);
+            Int32 bytesRead = stream.Read(buffer, 0, size);
 
             if (bytesRead != size)
                 throw new InvalidDataException();
@@ -23,22 +24,21 @@ namespace AudioSwitcher.IO
 
             Marshal.Copy(buffer, 0, ptr, size);
 
-            T result = (T)Marshal.PtrToStructure(ptr, typeof(T));
+            var result = (T)Marshal.PtrToStructure(ptr, typeof(T));
 
             Marshal.FreeHGlobal(ptr);
 
             return result;
         }
 
-        public static void Write<T>(this Stream stream, T value) where T : struct
-        {
+        static public void Write<T>(this Stream stream, T value) where T : struct {
             if (stream == null)
                 throw new ArgumentNullException(nameof(stream));
 
             int size = Marshal.SizeOf(typeof(T));
             byte[] buffer = new byte[size];
             IntPtr ptr = Marshal.AllocHGlobal(size);
-            Marshal.StructureToPtr(value, ptr, true);
+            Marshal.StructureToPtr((object)value, ptr, true);
             Marshal.Copy(ptr, buffer, 0, size);
             Marshal.FreeHGlobal(ptr);
             stream.Write(buffer, 0, size);
