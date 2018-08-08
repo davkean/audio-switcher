c1a9c0533265e801982b6a3fcae0797e4e2c554d
David Kean <davkean@microsoft.com>
2018-08-08 11:26:27 +1000

    Add excluded file.

---
+++ src/AudioSwitcher/Program - Copy.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) David Kean.
+// -----------------------------------------------------------------------
+using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using AudioSwitcher.ApplicationModel;
+
+namespace AudioSwitcher
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+
+            using (var container = new CompositionContainer(catalog))
+            {
+                IApplication application = container.GetExportedValue<IApplication>();
+                application.Start();
+            }
+        }
+    }
+}
